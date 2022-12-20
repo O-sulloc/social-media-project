1372c5b23fef9ea11449516f37c44908a67eb11e
O-sulloc <bujjaf@gmail.com>
2022-12-20 15:07:36 +0900

    update

--- src/main/java/com/app/socialmedia/controller/HelloController.java
+++ src/main/java/com/app/socialmedia/controller/HelloController.java
@@ -1,15 +1,18 @@
 package com.app.socialmedia.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @GetMapping("/api/v1/hello")
     public String hello() {
-        return "bye";
+        log.info("crontab 실행");
+        return "hello";
     }
 
     @GetMapping("/api/v1/hello2")
