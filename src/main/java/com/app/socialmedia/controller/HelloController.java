0edf048f3fa9ff232b0156adc94afee066e59b0f
O-sulloc <bujjaf@gmail.com>
2022-12-23 21:02:17 +0900

    hello controller update

--- src/main/java/com/app/socialmedia/controller/HelloController.java
+++ src/main/java/com/app/socialmedia/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @GetMapping("/api/v1/hello")
     public String hello() {
-        return "hello";
+        return "ci/cd test";
     }
 
     @GetMapping("/api/v1/hello2")
