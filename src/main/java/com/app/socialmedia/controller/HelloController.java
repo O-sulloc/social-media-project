baebc6f59c7d043d4a995b4f322c1395e5609b79
O-sulloc <bujjaf@gmail.com>
2022-12-27 14:48:48 +0900

    chore: Test build

--- src/main/java/com/app/socialmedia/controller/HelloController.java
+++ src/main/java/com/app/socialmedia/controller/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @GetMapping("/api/v1/hello")
     public String hello() {
-        return "김정현";
+        return "김정현test";
     }
 
     @GetMapping("/api/v1/hello2")
