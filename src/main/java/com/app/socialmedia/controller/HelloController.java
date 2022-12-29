746d433ca58fb6fc35a63af7df44455b9643d0bd
O-sulloc <bujjaf@gmail.com>
2022-12-29 10:05:34 +0900

    feat: Sum of digit

--- src/main/java/com/app/socialmedia/controller/HelloController.java
+++ src/main/java/com/app/socialmedia/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.app.socialmedia.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,17 @@ public class HelloController {
     @GetMapping("/api/v1/hello")
     public String hello() {
         return "김정현";
+    }
+
+    @GetMapping("/api/v1/{num}")
+    public Long hello2(@PathVariable Long num) {
+        //sum of digit
+        Long answer = 0L;
+
+        while(num > 0) {
+            answer += num % 10;
+            num /= 10;
+        }
+        return answer;
     }
 }
